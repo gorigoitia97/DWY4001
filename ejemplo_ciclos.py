67ed5684387d38a43adf8ffe21f6f219025e487f
Patricio <pat.gorigoitia@alumnos.duoc.cl>
2019-09-23 12:48:07 -0300

    Agregamos un ejemplo de uso ciclos

---
+++ ejemplo_ciclos.py
@@ -0,0 +1,15 @@
+# guardar como 'ejemplo_ciclo.py'
+
+# La sintaxis en python es diferente a lo que 
+# estamos acostumbrados... aqui los espacios SI importan.
+# tampoco es necesario declarar variables. Simplemente las usamos
+
+i = 6
+while i > 0 :
+    print(i)
+    i = i - 1
+
+ciudades = ["Arica", "Iquique", "Antofagasta", "Copiapo"]
+
+for ciudad in ciudades:
+    print(ciudad)
